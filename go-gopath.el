a3bcd4178bec593ef2932ebb0fa822085bebcbdc
Andrew Kirilenko <iced@gojuno.com>
2016-03-10 14:11:58 +0300

    Adding license.

--- go-gopath.el
+++ go-gopath.el
@@ -1,3 +1,33 @@
+;;; go-gopath.el --- Will guess GOPATH using gb and projectile.
+
+;; Author: Andrew Kirilenko <andrew.kirilenko.main@gmail.com>
+;; URL: http://github.com/iced/go-gopath/
+
+;;; License
+
+;; Copyright (c) 2016 Andrew Kirilenko
+
+;; Permission is hereby granted, free of charge, to any person
+;; obtaining a copy of this software and associated documentation
+;; files (the "Software"), to deal in the Software without
+;; restriction, including without limitation the rights to use, copy,
+;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;; of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+;; BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+;; ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
+
 (defun go-gopath-gb-env-line (name)
   (unless (eq buffer-file-name nil)
     (let ((gbe-env (split-string (shell-command-to-string "gb env") "\n")))
